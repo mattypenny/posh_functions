1832a42b84ecbab60e059db9547070d4fc77bebf
mattypenny <mattypenny@outlook.com>
2015-07-13 17:47:18 +0100

    Converted to using c:\temp\pins.xml

--- function-get-pins.ps1
+++ function-get-pins.ps1
@@ -1,68 +1,95 @@
 ﻿<#
 .Synopsis
-   Gets pins (i.e. saved URLs) from Pinboard
+  Gets pins (i.e. saved URLs) from Pinboard
 .DESCRIPTION
-   Gets pins (i.e. saved URLs) from Pinboard
+  Gets pins (i.e. saved URLs) from Pinboard
 
-   Todo: option parameter
-   Todo: tags parameter
+  Todo: option parameter
+  Todo: tags parameter
 
 .PARAMETER Username
-   Pinboard user. If the $PinUser variable has been defined, this will be used as a default
+  Pinboard user. If the $PinUser variable has been defined, this will be used as a default
 
 .PARAMETER Password
-   Pinboard password. If the $PinPass variable has been defined, this will be used as a default
+  Pinboard password. If the $PinPass variable has been defined, this will be used as a default
 
 .PARAMETER Option
-   Not yet implemented
-   The Pinboard API retrieval option. One of:
-      get - one or more posts on a single day
-      recent - most recent posts
-      dates - a list of dates with the number of posts at each date
-      all - all bookmarks in the user's account
+  Not yet implemented
+  The Pinboard API retrieval option. One of:
+    get - one or more posts on a single day
+    recent - most recent posts
+    dates - a list of dates with the number of posts at each date
+    all - all bookmarks in the user's account
 
 .PARAMETER Tags
-   Not yet implemented
-   Pinboard tags - up to 3
-   
+  Not yet implemented
+  Pinboard tags - up to 3
+  
 .EXAMPLE
    
 #>
 
 function get-pins
 {
-    [CmdletBinding()]
-    [OutputType([xml])]
-    Param
-    (
-        [string]$username = "$PinUser",
-        [string]$password = "$PinPass",
-        [string]$option = "recent",
-        [int]$count = 100
-    )
+  [CmdletBinding()]
+  [OutputType([xml])]
+  Param
+  (
+    
+    [string]$username = "$PinUser",
+    [string]$password = "$PinPass",
+    [string]$option = "recent",
+    [int]$count = 100
+  )
 
     
     
-    if ( $option -eq "recent" )
-    {
-       $uri = "https://api.pinboard.in/v1/posts/recent?count=$count"
-    }
-    else
-    {
-       $uri = "https://api.pinboard.in/v1/posts/$option"
-    }
+  if ( $option -eq "recent" )
+  {
+    $uri = "https://api.pinboard.in/v1/posts/recent?count=$count"
+  }
+  else
+  {
+    $uri = "https://api.pinboard.in/v1/posts/$option"
+  }
        
-    write-verbose "Uri: $uri"
+  write-verbose "Uri: $uri"
     
-    $secpasswd = ConvertTo-SecureString $pass -AsPlainText -Force
+  $secpasswd = ConvertTo-SecureString $pass -AsPlainText -Force
     
-    $cred = New-Object System.Management.Automation.PSCredential ($user, $secpasswd)
+  $cred = New-Object System.Management.Automation.PSCredential ($user, $secpasswd)
     
-    [xml]$PinsAsXml = Invoke-RestMethod -Uri $uri -Method Post -Credential $cred 
+  try
+  {
 
-    $PinsAsObject = $xml.posts.post
+    write-verbose "getting bookmarks with...."
+    write-verbose "Invoke-RestMethod -Uri $uri -Credential $cred -timeout 300 -outfile c:\temp\pins.xml"
+
+    Invoke-RestMethod -Uri $uri -Credential $cred -timeout 300 -outfile c:\temp\pins.xml
+
+    $PinsAsXml = gc c:\temp\pins.xml
+    write-verbose $($PinsAsXml | measure-object).count
     
-    return $PinsAsObject
+  }
+  catch 
+  {
+    Write-Host $_ -fore red
+    Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
+    Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
+  }
+
+  write-verbose "Converting xml to Powershell object"
+  $PinsAsObject = ""
+  $PinsAsObject = $xml.posts.post
+  # $PinsAsObject = Select-Xml -Content $xml -XPath "//href"
+  if ( $VerbosePreference -ne "SilentlyContinue" )
+  {
+    $NumberOfPins = $PinsAsObject | measure-object | select count
+    [str]$NumberOfPinsString = $NumberOfPins.count
+    write-verbose "Got this many bookmarks: $NumberOfPinsString"
+  }
+
+  return $PinsAsObject
 }
 
 
@@ -70,29 +97,29 @@ function get-pins
 .Synopsis
    This retrieves recent pins as webpage, with specfied tags as headers
 .DESCRIPTION
-   This function outputs the last week's Pinboard tags as html. 
+  This function outputs the last week's Pinboard tags as html. 
 
-   It takes a list of Pinboard tags as a parameter. The Pins for each tag are listed nder an <h3> level header, then any remaining tags are listed under a heading of 'other'.
+  It takes a list of Pinboard tags as a parameter. The Pins for each tag are listed nder an <h3> level header, then any remaining tags are listed under a heading of 'other'.
 
-   The Pinboard username and password can be supplied as a parameter, or for convenience, these can be set up in the Shell variables $PinUser and $PinPass.
+  The Pinboard username and password can be supplied as a parameter, or for convenience, these can be set up in the Shell variables $PinUser and $PinPass.
 
 .EXAMPLE
-   Example of how to use this cmdlet
+  Example of how to use this cmdlet
 .EXAMPLE
-   Another example of how to use this cmdlet
+  Another example of how to use this cmdlet
 #>
 function Show-PinsAsWebPage
 {
-    [CmdletBinding()]
-    Param
-    (
-        [string]$username = "$PinUser",
-        [string]$password = "$PinPass",
-        $tagList
-    )
+  [CmdletBinding()]
+  Param
+  (
+    [string]$username = "$PinUser",
+    [string]$password = "$PinPass",
+    $tagList
+  )
 
-    $pins = get-pins -username $username -password $secpasswd
-    convert-pinstowebpage $pins, $TagList
+  $pins = get-pins -username $username -password $secpasswd
+  convert-pinstowebpage $pins, $TagList
 
     
 
@@ -100,74 +127,74 @@ function Show-PinsAsWebPage
 
 function Convert-PinsToWebPage
 {
-    [CmdletBinding()]
-    Param
-    (
-        $pins,
-        $taglist
-    )
+  [CmdletBinding()]
+  Param
+  (
+    $pins,
+    $taglist
+  )
 
-    write-debug "$pins"
+  write-verbose "$pins"
 
+  foreach ($tag in $tagList)
+  {
+    write-verbose "$Tag"
+
+    # Todo: Need to allow for one tag incorporating another e.g. 'sql' and 'sqlserver'
+    [string]$TagString = $Tag
+    write-output "<h3>$TagString</h3>"
+    $PinsWithMatchingTag = $pins | ? tag -like "*$TagString*"
+
+    foreach ($Pin in $PinsWithMAtchingTag)
+    {
+      $Href = $Pin.href
+      $Description = $Pin.Description
+      write-output "<a href=`"$href`">$Description</a>"
+      write-output "<br>"
+    }
+  }
+
+  write-output "<h3>Other</h3>"
+
+  foreach ($pin in $pins)
+  {
+
+    $PinHasNoMatchingTags = $True
+
+    $Href = $Pin.href
+    $Description = $Pin.Description
+    $Extended = $Pin.Extended
+    $PinTag = $pin.tag 
+       
+    write-verbose "$Href" 
+    write-verbose "$Description" 
+    write-verbose "$PinTag" 
+       
     foreach ($tag in $tagList)
     {
-        write-debug "$Tag"
+      write-verbose "$Tag"
 
-        # Todo: Need to allow for one tag incorporating another e.g. 'sql' and 'sqlserver'
-        [string]$TagString = $Tag
-        write-output "<h3>$TagString</h3>"
-        $PinsWithMatchingTag = $pins | ? tag -like "*$TagString*"
-
-	foreach ($Pin in $PinsWithMAtchingTag)
-        {
-                $Href = $Pin.href
-                $Description = $Pin.Description
-		write-output "<a href=`"$href`">$Description</a>"
-		write-output "<br>"
-        }
-    }
-
-    write-output "<h3>Other</h3>"
-
-    foreach ($pin in $pins)
-    {
-
-        $PinHasNoMatchingTags = $True
-
-        $Href = $Pin.href
-        $Description = $Pin.Description
-        $Extended = $Pin.Extended
-        $PinTag = $pin.tag 
-       
-        write-debug "$Href" 
-        write-debug "$Description" 
-        write-debug "$PinTag" 
-       
-        foreach ($tag in $tagList)
-        {
-            write-debug "$Tag"
-
-            # Todo: Need to allow for one tag incorporating another e.g. sql and sqlserver
-            [string]$TagString = $Tag
+      # Todo: Need to allow for one tag incorporating another e.g. sql and sqlserver
+      [string]$TagString = $Tag
       
-            if ( $PinTag -like "*$TagString*" )
-            {
-                $PinHasNoMatchingTags = $False
-            }
-
-        }
-
-        if ( $PinHasNoMatchingTags -eq $True )
-        {
-	    write-output "<a href=`"$href`">$Description</a>"
-            if ( $Extended -ne "" )
-            {
-	    	write-output "- $Extended"
-            }
-	    write-output "<br>"
-        }
+      if ( $PinTag -like "*$TagString*" )
+      {
+        $PinHasNoMatchingTags = $False
+      }
 
     }
+
+    if ( $PinHasNoMatchingTags -eq $True )
+    {
+      write-output "<a href=`"$href`">$Description</a>"
+      if ( $Extended -ne "" )
+      {
+        write-output "- $Extended"
+      }
+      write-output "<br>"
+    }
+
+  }
 
 }
 
@@ -176,3 +203,5 @@ function Convert-PinsToWebPage
 $Posts = $xml.posts.post | ? tag -notlike "*powershell*" | ? tag -notlike "*sqlserver*" | ? tag -notlike "*gtd*"
 $Posts | ft @{Expression = {"<a href=`"" + $_.href + "`">" + $_.description + "</a> " + $_.Extended }}
 #>
+
+# vim: set softtabstop=2 shiftwidth=2 expandtab 
