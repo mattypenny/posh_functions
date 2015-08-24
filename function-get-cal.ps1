aef297205b231365115b67288a5d5281b14eab4d
mattypenny <mattypenny@outlook.com>
2015-08-24 13:33:06 +0100

    Amended spacing, added debuggery

--- function-get-cal.ps1
+++ function-get-cal.ps1
@@ -6,19 +6,16 @@
 function get-cal { 
 <#
 .SYNOPSIS
-Unix Cal Command ( Get-Cal.ps1 ) [sh]
+Similar to the Unix Cal Command ( Get-Cal.ps1 ). Lightly tweaked version of a acript by 'xshell guest' 
 
 .DESCRIPTION
-Downloaded from the internet:
-http://www.vistax64.com/powershell/17834-unix-cal-command.html
+Similar to the Unix Cal Command ( Get-Cal.ps1 ). This is a lightly tweaked version of a acript by someone with the handle 'xshell guest' that I found at http://www.vistax64.com/powershell/17834-unix-cal-command.html 
 
-
-
+It gives you a nicely formatted calendar view of the month or year
 
 .PARAMETER Year
+
 .PARAMETER Month
-
-
 
 .INPUTS
 None. You cannot pipe objects to this function
@@ -36,7 +33,7 @@ Su Mo Tu We Th Fr Sa
 27 28 29 30 31
 
 .EXAMPLE
-cal 2013 12
+get-cal 2013 12
 
     December 2013
 
@@ -73,76 +70,94 @@ Su Mo Tu We Th Fr Sa
 
 
 .LINK
-Twiki list: http://ourwiki/twiki501/bin/view/Main/DBA/PowershellFunctions
+Twiki list: http://www.vistax64.com/powershell/17834-unix-cal-command.html 
 
 
 #>
   [CmdletBinding()]	
 
-Param(
-[Int]$Year=(Get-Date).Year,
-[Int]$Month=(Get-Date).Month,
-[Switch] $Next,
-[Switch] $Previous
-)
-$script:err=$false
-if ($Next)
-{
-$Month=((Get-Date).Month+1)
-if ($Month -eq 13)
-{
-$Year=(Get-Date).Year+1
-$Month=1
-}
-}
-if ($Previous)
-{
-$Month=((Get-Date).Month-1)
-if ($Month -eq 0)
-{
-$Year=(Get-Date).Year-1
-$Month=12;
-}
-}
-$Space=" "
-trap { Write-Host -f red "Exception occurred while parsing month/year
-parameters.";$script:err=$true;continue}
-$StartDate=new-object System.DateTime $Year,$Month,1
-if ($script:err)
-{
-return;
-}
-$MonthLabel="$($StartDate.ToString('MMMM')) $($StartDate.Year)"
-$DOWLabel=[Enum]::GetValues([DayOfWeek]) | % {
-$_.ToString().SubString(0,2) }
-$AlignCenter=[Math]::Ceiling((20-$MonthLabel.Length)/2)
-Write-Host
-"{0}{1}" -f ($Space * $AlignCenter),$MonthLabel
-Write-Host
-"$DOWLabel"
-$NextDate=$StartDate
-$SpaceCount=(([int]$StartDate.DayOfWeek))*3
-Write-Host $($Space * $SpaceCount) -No
-While ($NextDate.Month -eq $StartDate.Month)
-{
-if ($NextDate.ToString("MMddyyyy") -eq $(get-date).ToString("MMddyyyy"))
-{
-Write-Host ("{0,2}" -f $NextDate.Day) -no -fore yellow
-}
-else
-{
-Write-Host ("{0,2}" -f $NextDate.Day) -no
-}
-Write-Host " " -No
-$NextDate=$NextDate.AddDays(1)
-if ($NextDate.DayOfWeek -eq [DayOfWeek]::Sunday)
-{
-Write-Host
-}
+  Param(
+    [Int]$Year=(Get-Date).Year,
+    [Int]$Month=(Get-Date).Month,
+    [Switch] $Next,
+    [Switch] $Previous
+  )
 
-}
-Write-Host
-  
+
+  $script:err=$false
+  if ($Next)
+  {
+    $Month=((Get-Date).Month+1)
+    if ($Month -eq 13)
+    {
+      $Year=(Get-Date).Year+1
+      $Month=1
+    }
+  }
+  if ($Previous)
+  {
+    $Month=((Get-Date).Month-1)
+    if ($Month -eq 0)
+    {
+      $Year=(Get-Date).Year-1
+      $Month=12;
+    }
+  }
+  $Space=" "
+
+  trap { Write-Host -f red "Exception occurred while parsing month/year parameters.";$script:err=$true;continue}
+
+  write-debug "After trap statenebt"
+
+  $StartDate=new-object System.DateTime $Year,$Month,1
+  if ($script:err)
+  {
+    return;
+  }
+  write-debug "`$StartDate: $StartDate"
+
+  $MonthLabel="$($StartDate.ToString('MMMM')) $($StartDate.Year)"
+  write-debug "`$MonthLabel: $MonthLabel"
+
+  $DOWLabel=[Enum]::GetValues([DayOfWeek]) | % { $_.ToString().SubString(0,2) }
+  write-debug "`$DOWLabel: $DOWLabel"
+
+  $AlignCenter=[Math]::Ceiling((20-$MonthLabel.Length)/2)
+  write-debug "`$AlignCenter: $AlignCenter"
+
+  "{0}{1}" -f ($Space * $AlignCenter),$MonthLabel
+  write-debug "Written {0}{1} construct"
+
+  Write-Host "$DOWLabel"
+  write-debug "Written $DOWLabel"
+
+  $NextDate=$StartDate
+  write-debug "`$NextDate: $NextDate"
+
+  $SpaceCount=(([int]$StartDate.DayOfWeek))*3
+  write-debug "`$SpaceCount: $SpaceCount"
+
+  Write-Host $($Space * $SpaceCount) -No
+
+  While ($NextDate.Month -eq $StartDate.Month)
+  {
+    if ($NextDate.ToString("MMddyyyy") -eq $(get-date).ToString("MMddyyyy"))
+    {
+      Write-Host ("{0,2}" -f $NextDate.Day) -no -fore yellow
+    }
+    else
+    {
+      Write-Host ("{0,2}" -f $NextDate.Day) -no
+    }
+    Write-Host " " -No
+    $NextDate=$NextDate.AddDays(1)
+    if ($NextDate.DayOfWeek -eq [DayOfWeek]::Sunday)
+    {
+      Write-Host
+    }
+
+  }
+  Write-Host
 
 }
 set-alias cal get-cal
