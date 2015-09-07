0f23a8ca48a8ff9bb04f1069406968d12eebb918
mattypenny <mattypenny@gmail.com>
2015-09-07 14:21:45 +0100

    Embelished the show function. Converted to get-, show-, alias format

--- function-show-serversize.ps1
+++ function-show-serversize.ps1
@@ -3,12 +3,18 @@
 # ----------------------------------------------------------------------
 function get-serversize { Param( [String] $ComputerName)
 
-Get-WMIObject Win32_LogicalDisk -filter "DriveType=3" -computer $ComputerName | 
-   Select SystemName, DeviceID, VolumeName,
-          @{Name="size(GB)";Expression={"{0:N1}" -f($_.size/1gb)}},
-          @{Name="freespace(GB)";Expression={"{0:N1}" -f($_.freespace/1gb)}} 
+Get-WMIObject Win32_LogicalDisk -filter "DriveType=3" -computer $ComputerName 
 }
+set-alias gs get-serversize
 
-function ss { Param( [String] $ComputerName)
-    get-serversize $ComputerName | ft
+function show-serversize { Param( [String] $ComputerName)
+    get-serversize $ComputerName |  
+      Select SystemName, 
+             DeviceID, 
+             VolumeName,
+             @{Name="size(GB)";Expression={"{0:N1}" -f($_.size/1gb)}},
+             @{Name="freespace(GB)";Expression={"{0:N1}" -f($_.freespace/1gb)}}, 
+             @{Name="used(GB)";Expression={"{0:N1}" -f(($_.size - $_.freespace)/1gb)}},
+             @{Name="%used";Expression={"{0:N1}" -f((($_.size - $_.freespace)/$_.size) * 100)}} | ft -a
 }
+set-alias ss show-serversize
