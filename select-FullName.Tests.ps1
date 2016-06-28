0fe9229e5b6164374a9a3c8bc31cfc2a03313c80
Matt Penny <mpenny@simplyhealth.co.uk>
2016-06-28 16:55:20 +0100

    not working yet, but valuefrompipeline added

---
+++ select-FullName.Tests.ps1
@@ -0,0 +1,25 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "select-FullName" {
+    $PesterData = "c:\powershell\functions\PesterData"
+    It "displays the fullname for a single specified file" {
+        select-fullname $PesterData\existent-file.dat | Should Be "c:\powershell\functions\PesterData\existent-file.dat"
+    }
+<#
+    It "displays the fullname for a multiple files" {
+        $true | Should Be $false
+    }
+    It "generates an error if the file doesn't exist" {
+        $true | Should Be $false
+    }
+#>
+    It "displays the fullname if you pipe in output from get-childitem" {
+        $true | Should Be $false
+    }
+    It "displays nothing if you pipe in nothing" {
+        $true | Should Be $false
+    }
+
+}
---
+++ select-FullName.ps1
@@ -0,0 +1,37 @@
+﻿function select-FullName {
+
+  Param ( 
+    [Parameter(Mandatory=$False,ValueFromPipeLine=$true, Position=1)][String]$filename = "."
+  )
+
+  foreach ($i in $input)
+  { 
+    $I | select fullname
+  }
+<#
+    if ($input)
+    {
+      foreach ($i in $input)
+      { 
+        $filename = $i
+      
+        $ChildItem = get-childitem $filename 
+
+        [string]$FullName = $ChildItem.FullName
+
+        return $FullName
+    }
+    else
+    {
+        $ChildItem = get-childitem $filename 
+
+        [string]$FullName = $ChildItem.FullName
+
+        return $FullName
+
+
+    }
+  }
+#>
+
+}
