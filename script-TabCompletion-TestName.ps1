f8356d1e76c612efb0fb11f5faba3d26ab97cdd6
mattypenny <mattypenny@outlook.com>
2016-07-27 18:49:10 +0100

    initial version of tab completion

---
+++ script-TabCompletion-TestName.ps1
@@ -0,0 +1,31 @@
+﻿$Completion_TestName = {
+
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    $searcher = select-string "^Describe `"*`"*`{*" *.Tests.ps1
+
+    $searcher | ForEach-Object {
+
+        New-Object -TypeName pscustomobject -Property @{ 
+            TestName = $_.line -replace 'Describe',''  -replace '{','' 
+        }
+
+    } | Sort-Object TestName | ForEach-Object {
+
+        New-Object System.Management.Automation.CompletionResult $_.TestName
+
+    }
+
+}
+
+ 
+
+ 
+
+if (-not $global:options) { $global:options = @{CustomArgumentCompleters = @{};NativeArgumentCompleters = @{}}}
+
+ 
+
+$global:options['CustomArgumentCompleters']['TestName'] = $Completion_TestName
+
+$function:tabexpansion2 = $function:tabexpansion2 -replace 'End\r\n{','End { if ($null -ne $options) { $options += $global:options} else {$options = $global:options}' 
