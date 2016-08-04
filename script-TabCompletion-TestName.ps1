9e42a60866fe57869655bf294cb62a21b0b0fff3
mattypenny <mattypenny@outlook.com>
2016-08-04 09:00:43 +0100

    added comments. Ctrl-space ok in script pane

--- script-TabCompletion-TestName.ps1
+++ script-TabCompletion-TestName.ps1
@@ -1,7 +1,15 @@
 ﻿$Completion_TestName = {
 
+    <#
+        Based on http://www.powershellmagazine.com/2012/11/29/using-custom-argument-completers-in-powershell-3-0/
+    #>
+
+    # not sure what the last two parameters are here, tbh
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
 
+    <#
+        Identifying any lines which begin with 'Describe {' are Pester describe lines
+    #>
     $DescribeLines = select-string "^Describe `"*`"*`{*" *.Tests.ps1 | sort-object -Property Line
 
     $PesterTests = foreach ($Line in $DescribeLines) 
@@ -10,7 +18,7 @@
 
         $TestDescription = $TestDescription.trim()
 
-        
+        # if the test name includes a space then it needs to show up in the tab completion in quotes
         if ($TestDescription -match ' ')
         {
             # $TestDescription = "'" + $TestDescription + "'"
@@ -33,7 +41,7 @@
 
  
 
-
+# these lines copied from the t'internet sa
 if (-not $global:options) { $global:options = @{CustomArgumentCompleters = @{};NativeArgumentCompleters = @{}}}
 
 $global:options['CustomArgumentCompleters']['TestName'] = $Completion_TestName
