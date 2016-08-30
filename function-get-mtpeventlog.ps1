ccab56ca489fa9aec59b6a37a9ae4f203a719330
Matt Penny <mattypenny@gmail.com>
2016-08-30 15:59:22 +0100

    Update function-get-mtpeventlog.ps1

--- function-get-mtpeventlog.ps1
+++ function-get-mtpeventlog.ps1
@@ -22,25 +22,6 @@ Go back this number of days. Defaults to 0, so today
 None. You cannot pipe objects to this function
 
 .EXAMPLE
-gel  -starttime 08:00 -NoSecurity  -Minutes 15 -computername achimg006 -daysago 0
-
-   Index Time          EntryType   Source                 InstanceID Message
- 1858476 Aug 30 08:13  Information Kofax Capture Export            0 Service started successfully.
- 1858475 Aug 30 08:12  Information Kofax Capture Export            0 Service stopped successfully.
- 1858474 Aug 30 08:12  Error       Userenv                3221226557 The Group Policy client-side extension Internet Explorer Zonemapping failed to execute. Please look for any errors reported earlier by that extension.
-get-eventlog : No matches found
-At C:\powershell\functions\function-get-mtpeventlog.ps1:60 char:9
-+         get-eventlog -computername $ComputerName -logname $L.Log -before $EndTim ...
-+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    + CategoryInfo          : ObjectNotFound: (:) [Get-EventLog], ArgumentException
-    + FullyQualifiedErrorId : GetEventLogNoEntriesFound,Microsoft.PowerShell.Commands.GetEventLogCommand
- 
-get-eventlog : No matches found
-At C:\powershell\functions\function-get-mtpeventlog.ps1:60 char:9
-+         get-eventlog -computername $ComputerName -logname $L.Log -before $EndTim ...
-+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    + CategoryInfo          : ObjectNotFound: (:) [Get-EventLog], ArgumentException
-    + FullyQualifiedErrorId : GetEventLogNoEntriesFound,Microsoft.PowerShell.Commands.GetEventLogCommand 
 
 #>
 [CmdletBinding()]
